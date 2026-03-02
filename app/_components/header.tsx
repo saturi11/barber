3327733bfc846536d358864368f9cd9d91372d4c
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-03-02 15:16:55 -0300

    feat: add header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex items-center justify-between p-5">
+        <Image alt="Logo" src="/logo.png" width={120} height={18} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
 "use client"
 
-import { useEffect } from "react"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-const name = "Gabriel"
-const name2 = "Gabriel"
 const Home = () => {
-  return <Button>Home</Button>
+  return (
+    <div>
+      {/*header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Gabriel</h2>
+        <p>Segunda-feira, 2 de marco</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faca sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="banner"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
