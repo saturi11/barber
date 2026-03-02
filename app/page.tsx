5b829141226e5096c51e6bed2d77886cd08302c9
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-03-02 12:34:47 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 import { Button } from "./_components/ui/button"
 
 const name = "Gabriel"
-
+const name2 = "Gabriel"
 const Home = () => {
   return <Button>Home</Button>
 }
