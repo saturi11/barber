3bff1dcda0ab800895d9b1179bc9f5b229b2c0c7
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-03-05 14:38:34 -0300

    feat: implement booking functionality with calendar and service selection

---
+++ app/_actions/create-booking.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
+
+interface CreateBookingParams {
+  serviceId: string
+  date: Date
+}
+
+export const createBooking = async (params: CreateBookingParams) => {
+  const session = await getServerSession(authOptions)
+
+  if (!session?.user) {
+    throw new Error("Usuário não autenticado")
+  }
+
+  await db.booking.create({
+    data: {
+      ...params,
+      userId: session.user.id,
+    },
+  })
+
+  revalidatePath("/barbershops/[id]")
+  revalidatePath("/bookings")
+}
